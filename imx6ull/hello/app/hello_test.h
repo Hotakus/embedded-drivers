3a233bf437cc9244d084a52bb4f99c335fe51d46
Hotakus <ttowfive@gmail.com>
2023-09-19 07:44:31 +0800

    refactor(imx6ull) imx6ull -> drv

--- imx6ull/hello/app/hello_test.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by hotakus on 9/18/23.
-//
-
-#ifndef HELLO_HELLO_TEST_H
-#define HELLO_HELLO_TEST_H
-
-#endif //HELLO_HELLO_TEST_H
--- imx6ull/hello/drv/hello.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by hotakus on 9/15/23.
-//
-
-#ifndef HELLO_HELLO_H
-#define HELLO_HELLO_H
-
-#endif //HELLO_HELLO_H
